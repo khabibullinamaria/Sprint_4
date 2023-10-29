afc9e392c91c5f49c7ff273f540e53c0a2fe3405
Maria Golovina <marykhabibullina@gmail.com>
2023-10-29 21:38:53 +0500

    Code has been fixed one more time, really ready to review

--- src/main/java/pageobjects/BaseTest.java
+++ src/main/java/pageobjects/BaseTest.java
@@ -3,8 +3,8 @@ package pageobjects;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -12,9 +12,9 @@ public class BaseTest {
 
     @Before
     public void beforeOrder() {
-        FirefoxOptions options = new FirefoxOptions();
+        ChromeOptions options = new ChromeOptions();
         //options.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
-        driver = new FirefoxDriver(options);
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://qa-scooter.praktikum-services.ru/");
     }
--- src/test/java/example/OrderSamokat.java
+++ src/test/java/example/OrderSamokat.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.WebDriver;
 import pageobjects.*;
 
 //Класс с автотестом
